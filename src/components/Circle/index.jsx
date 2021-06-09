bb5e3649549035d2f50d44b8f69dee20babf2f13
ljubica-work <85488757+ljubica-work@users.noreply.github.com>
2021-06-09 11:37:07 +0200

    banner (#1)
    Co-authored-by: ljubica <apple@Apples-MacBook-Pro.local>

---
+++ src/components/Circle/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Circle.scss';
+
+const Circle = () => {
+  return (
+    <div className='circle circle--dark circle--banner'></div>
+  );
+}
+
+export default Circle;
