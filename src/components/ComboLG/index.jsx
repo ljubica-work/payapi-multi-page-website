503fb259f604852578bc798b8a6f3ef3845921e1
ljubica <apple@Apples-MacBook-Pro.local>
2021-06-09 17:27:48 +0200

    png to svg change

--- src/components/ComboLG/index.jsx
+++ src/components/ComboLG/index.jsx
@@ -5,8 +5,9 @@ import cx from 'classnames';
 
 import './ComboLG.scss';
 
-import codescreen from '../../assets/images/code-screen.png';
-import iphonemockup from '../../assets/images/iphone-mockup.png';
+import { ReactComponent as Codescreen} from '../../assets/svg/code-screen.svg';
+import { ReactComponent as Iphonemockup } from '../../assets/svg/iphone-mockup.svg';
+
 
 const ComboLG = ({type, reverse}) => {
 
@@ -17,11 +18,10 @@ const ComboLG = ({type, reverse}) => {
 
   let titleText = (type === 'code') ? 'Easy to implement' : 'Simple UI & UX';
   let paragraphText = (type === 'code') ? 'Our API comes with just a few lines of code. You’ll be up and running in no time. We built our documentation page to integrate payments functionality with ease.' : 'Our pre-built form is easy to integrate in your app or website’s checkout flow and designed to optimize conversion.';
-  let image = (type === 'code') ? codescreen : iphonemockup;
   
   return (
     <div className={classes}>
-      <img src={image} alt='' className='combolg__image'/>
+      {type === 'code' ? <Codescreen className='combolg__image' /> : <Iphonemockup className='combolg__image' />}
       <div className='combolg__content'>
         <Title className='combolg__title' text={titleText}/>
         <Paragraph className='combolg_paragraph' text={paragraphText}/>
