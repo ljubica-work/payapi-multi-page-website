49a8e1d28b69f4ab252727d76b59bc73dac97c65
ljubica <apple@Apples-MacBook-Pro.local>
2021-06-10 13:46:41 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from './components/Banner';
 import ComboLarge from './components/ComboLarge';
 import ComboSmall from './components/ComboSmall';
+import Footer from './components/Footer';
 
 import './styles/index.scss';
 
@@ -12,6 +13,7 @@ const  App = () => {
       <ComboLarge type='code' title='Easy to implement' text='Our API comes with just a few lines of code. You’ll be up and running in no time. We built our documentation page to integrate payments functionality with ease.'/>
       <ComboLarge type='iphone' title='Simple UI & UX' text='Our pre-built form is easy to integrate in your app or website’s checkout flow and designed to optimize conversion.' reverse/>
       <ComboSmall />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import Circle from '../Circle';
+
+import { ReactComponent as Facebook } from '../../assets/svg/fb-icon.svg';
+import { ReactComponent as Twitter } from '../../assets/svg/tw-icon.svg';
+import { ReactComponent as Linkedin } from '../../assets/svg/in-icon.svg';
+import { ReactComponent as Logo } from '../../assets/svg/logo-light.svg';
+
+import './Footer.scss';
+
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <Circle className='footer__circle'/>
+      <div className='footer__wrapper'>
+        <div className='footer__menu'>
+            <Logo className='footer__logo'/>
+              <ul className='footer__list'>
+                <li className='footer__list-item'><a href='https://www.google.com/' className='footer__link'>Pricing</a></li>
+                <li className='footer__list-item'><a href='https://www.google.com/' className='footer__link'>About</a></li>
+                <li className='footer__list-item'><a href='https://www.google.com/' className='footer__link'>Contact</a></li>
+              </ul>
+        </div>
+
+        <div className='footer__icons'>
+          <a href='https://www.google.com/'><Facebook className='footer__icon'/></a>
+          <a href='https://www.google.com/'><Twitter className='footer__icon'/></a>
+          <a href='https://www.google.com/'><Linkedin className='footer__icon'/></a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
