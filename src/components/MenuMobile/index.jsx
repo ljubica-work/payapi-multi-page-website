8952a7d7ff9e8cb643451d5e38ce68862043ca08
ljubica-work <85488757+ljubica-work@users.noreply.github.com>
2021-06-17 16:25:14 +0200

    sidebar closes on click outside of it (#12)
    * sidebar closes on click outside of it
    
    * sidebar closing done with useCallback
    
    * close button changed from <div> to <button>
    
    * changed the name of the onClick function
    
    Co-authored-by: ljubica <apple@Apples-MacBook-Pro.local>

--- src/components/MenuMobile/index.jsx
+++ src/components/MenuMobile/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import cx from 'classnames';
 
 import Sidebar from '../Sidebar';
@@ -9,6 +9,31 @@ import './MenuMobile.scss';
 
 const MenuMobile = () => {
   const [open, setOpen] = useState(false);
+
+  const sidebarRef = useRef();
+  const menuButtonRef = useRef();
+
+  const handleCBodylick = useCallback(
+    (e) => {
+      if (
+        sidebarRef.current.contains(e.target) ||
+        menuButtonRef.current === e.target
+      ) {
+        return;
+      }
+      if (open) {
+        setOpen(false);
+      }
+    },
+    [open],
+  );
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleCBodylick);
+    return () => {
+      document.removeEventListener('mousedown', handleCBodylick);
+    };
+  }, [handleCBodylick]);
 
   const classes = cx({
     'mobile-menu__button': true,
@@ -28,10 +53,12 @@ const MenuMobile = () => {
   return (
     <div className='mobile-menu'>
       <Logo />
-      <div className={classes} onClick={handleClick}>
-        <div className='mobile-menu__burger'></div>
+      <button ref={menuButtonRef} className={classes} onClick={handleClick}>
+        <span className='mobile-menu__burger'></span>
+      </button>
+      <div ref={sidebarRef}>
+        <Sidebar open={open} />
       </div>
-      <Sidebar open={open} />
     </div>
   );
 };
